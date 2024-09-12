582a6f94172afa0402d4c82729ff4bf30b20302b
Jonathan <skyon.proper@gmail.com>
2024-09-12 16:15:22 +0300

    first commit

---
+++ src/GlobalData.jsx
@@ -0,0 +1,24 @@
+import Table from "react-bootstrap/Table";
+
+function GlobalData() {
+  return (
+    <Table striped bordered hover>
+      <tbody>
+        <tr>
+          <td>BTC</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Vol 24h</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Market Cap</td>
+          <td></td>
+        </tr>
+      </tbody>
+    </Table>
+  );
+}
+
+export default GlobalData;
