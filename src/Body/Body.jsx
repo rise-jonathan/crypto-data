89d873d13c1e14dde8e113067a2032d2f11e1919
Jonathan <80689642+rise-jonathan@users.noreply.github.com>
2024-09-20 18:58:12 +0300

    zoom - hide CoinPage

--- src/Body/Body.jsx
+++ src/Body/Body.jsx
@@ -2,16 +2,13 @@
 import CoinPage from "./CoinPage/CoinPage";
 import GlobalData from "./GlobalData";
 import ListCoins from "./ListCoins";
-// import ModalChart from "./ModalChart";
 
 const Body = () => {
   return (
     <>
       <GlobalData />
       <ListCoins />
-      <CoinPage />
-      {/* <CoinChart />
-      <ModalChart /> */}
+      {/* <CoinPage /> */}
     </>
   );
 };
