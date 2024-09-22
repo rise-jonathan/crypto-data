e1836d57bca692c0028a98ac2d9f608ef526b487
Jonathan <skyon.proper@gmail.com>
2024-09-22 19:23:55 +0300

    added fun to api.js and CoinMetrics);

--- src/Body/CoinPage/CoinMetrics.jsx
+++ src/Body/CoinPage/CoinMetrics.jsx
@@ -1,32 +1,54 @@
+import React from "react";
 import Table from "react-bootstrap/Table";
+import { getCoinInfo, getCoinMarketData } from "../../services/api";
+
+
 
 function CoinMetrics() {
+  const [coinInfo, setCoinInfo] = React.useState([]);
+  const [marketData, setMarketData] = React.useState([]);
+
+  React.useEffect(() => {
+
+    const coinId = "btc-bitcoin";
+
+    getCoinInfo(coinId).then(setCoinInfo);
+
+    getCoinMarketData(coinId).then(setMarketData);
+
+  }, []);
+
+  console.log(CoinMetrics)
+
   return (
     <>
-      <h3>Bitcoin (BTC) Metrics</h3>
+      <h3>{coinInfo.name} ({coinInfo.symbol}) Metrics</h3>
+
       <Table striped bordered hover>
         <tbody>
+
           <tr>
             <td>Market Cap</td>
-            <td></td>
+            <td>{marketData.rank}</td>
           </tr>
           <tr>
             <td>All Time High</td>
-            <td></td>
+            <td>$ {marketData.max_supply.toLocaleString('en-US').replace(/,/g, ' ')}</td>
           </tr>
           <tr>
             <td>Volume (24h)</td>
-            <td></td>
+            <td>${marketData.total_supply.toLocaleString('en-US').replace(/,/g, ' ')}</td>
           </tr>
           <tr>
             <td>Vol / M Cap (24h)</td>
-            <td></td>
+            <td>%{(marketData.volume_24h_usd)}</td>
           </tr>
           <tr>
             <td>Circulating Supply</td>
-            <td></td>
+            <td>{marketData.circulating_supply} {coinInfo.symbol}</td>
           </tr>
           <tr></tr>
+
         </tbody>
       </Table>
     </>
